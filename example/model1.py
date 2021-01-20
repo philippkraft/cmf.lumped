84be456802c480ad6fb70a3af97e4cff225540fe
philippkraft <philipp.kraft@umwelt.uni-giessen.de>
2021-01-20 20:10:23 +0100

    Updated docs for model1

--- example/model1.py
+++ example/model1.py
@@ -1,8 +1,39 @@
+"""
+CMF-Model for Nidder / Glauberg
+
+by Philipp Kraft
+
+last modification: 2021-01-20
+
+This text is from the doc-string of the Model's module
+"""
 
 import os
 import cmf
 from cmflumped.basemodel import BaseParameters, u, BaseModel
 from cmflumped.dataprovider import load_csv
+
+
+# The concept class is only used for documentation purposes.
+class Concept:
+    """
+    Concept for {name}
+    ==================
+
+    Using the concept of a simple overflowing bucket,
+    the model does not generate any baseflow.
+
+    It is based on ...
+
+    bla...
+
+    The implementation idea is shown in :numref:`fig_{name}_concept`
+
+    .. _fig_{name}_concept:
+    .. figure:: {name}.concept.png
+
+    """
+    ...
 
 
 class Parameters(BaseParameters):
@@ -36,9 +67,8 @@ class Model1(BaseModel):
     """
     CMF-Model for Nidder / Glauberg
 
-    by Philipp Kraft
-
-    last modification: 2021-01-13
+    A simple single storage model with infiltration and saturation excess
+    but no baseflow
     """
     verbose = True
     calibration_start = 2000
@@ -54,6 +84,7 @@ class Model1(BaseModel):
     def create_nodes(self):
         """
         Create the nodes (storages, distribution nodes and boundaries) of your model.
+
         Called from model initialization (self.__init__)
         """
         # Create two subsurface storages
@@ -64,7 +95,8 @@ class Model1(BaseModel):
 
     def create_result_structure(self):
         """
-        Creates the container to store the results in
+        Creates the container to store the results in.
+
         Called, when the run time loop starts
         """
         outflow = cmf.timeseries(self.data.begin, cmf.day)
@@ -73,9 +105,9 @@ class Model1(BaseModel):
 
     def fill_result_structure(self, result, t):
         """
-        Fills the result structure with data
+        Fills the result structure with data.
 
-        Called in each timestep
+        Called in each timestep.
         """
         q = self.output(t)
         result.add(q)
@@ -96,7 +128,6 @@ class Model1(BaseModel):
     def create_connections(self, p: Parameters):
         """
         Creates the connections and parameterizes the storages of the model
-
         """
         # Infiltration
         cmf.SimpleInfiltration(self.soil, self.cell.surfacewater, W0=p.infiltration_w0)
@@ -120,6 +151,7 @@ class Model1(BaseModel):
     def output(self, t):
         """
         Defines what the ouput of the model is
+
         :param t: Time step of the model
         :return: A value representing the model output
         """
